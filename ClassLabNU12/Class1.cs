71b852a684d0fec5ee9b4811ba1b87bb295ebda5
murillo.mvjesus <murillo.mvjesus@ITQ0626044W10-1>
2022-04-07 11:48:11 -0300

    Adicionar arquivos de projeto.

---
+++ ClassLabNU12/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLabNU12
+{
+    public class Produto
+    {
+
+    }
+
+      public Produto (string descrição,int valor )
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ ClassLabNU12/Produto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLabNU12
+{
+    
+    public class Produto
+    {
+
+        //atributos
+        private int id;
+        private string nome;
+        private string cpf; 
+        private string produto; 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
